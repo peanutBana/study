b94a34ba8bf264c21a5999f294de1d16daaf577f
peanutBana <diao0321@gmail.com>
2022-10-24 17:31:31 +0900

    register jsv파일 생성

---
+++ study/study/22.10.12/register/register.js
@@ -0,0 +1,55 @@
+const userid = document.querySelector('#userid');
+const pw1 = document.querySelector('#pw1');
+const pw2 = document.querySelector('#pw2');
+const level = document.querySelector('#level');
+const fullname = document.querySelector('#fullname');
+const email = document.querySelector('#email');
+const tel = document.querySelector('#tel');
+const submtitButton = document.querySelector('#submitButton');
+
+submtitButton.addEventListener('click',function(){
+    
+
+    // 비밀번호 : 공백여 / 특수문자, 문자, 숫자 포함 형태의 8~15자리입력
+
+    // 비밀번호 확인 : 공백여부 / 비밀번호와 일치 여부
+
+    // 이름 : 공백여부 / 
+
+    // 메일 주소 :  공백여부 / 메일형식에 맞는지
+    
+    // 연락처 : 필수값x => 공백여부x / 연락처 형식에 맞는지
+    
+    //span.textContent = 'alert_msg'
+})
+
+function idConfirm(){
+    if(!userid.value){
+        
+    }
+}
+
+
+function pwdConfirm(){
+
+}
+
+
+function pwd2Confirm(){
+
+}
+
+
+function fullnameConfirm(){
+
+}
+
+
+function emailConfirm(){
+
+}
+
+
+function telConfirm(){
+
+}
