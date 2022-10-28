7cc3761341c0b52f57770212d803c037672b7d98
peanutBana <diao0321@gmail.com>
2022-10-28 17:21:12 +0900

    비동기

--- study/study/22.10.28/accueweather/js/responsive.js
+++ study/study/22.10.28/accueweather/js/responsive.js
@@ -1,16 +1,32 @@
-//모바일 햄버거 버튼 클릭 시 열림
-$('.hamburger').click(function(){
-    $('.mobile.hamburger').hide()   //display : none
-    $('.mobile.close').show()   //display : block
+//모바일 햄버거 버튼 클릭시
+$('.hamburger').click(function() {
+    $('.mobile.hamburger').hide()   //display 속성이 none으로 바뀜
+    $('.mobile.close').show()   //display 속성이 block으로 바뀜
+
+    $('#mobile_menu').empty()
 
     var nav = $('.nav').clone()
 
-    $('#mobile_menu').append(nev)
-    $('#mobile_menu').show()
-
+    $('#mobile_menu').append(nav)   //append (내가 선택한 요소의 하위에 nav를 집어 넣음)
+    $("#mobile_menu").show();
 })
-//모바일 햄버거 버튼 닫기
-$('.close').click(function(){
-    $('.mobile.hamburger').show()   //display : block
-    $('.mobile.close').hide()    //display : none
+
+//모바일 햄버거 닫기 버튼 클릭시
+$('.close').click(function() {
+    $('.mobile.hamburger').show()   
+    $('.mobile.close').hide()   
+    $('#mobile_menu').hide()
+})
+
+//브라우저 리사이징
+$(window).resize(function(){
+    var width =  $(window).width()
+
+    if(width >= 767){
+        if($('#mobile_menu').is(':visible')){
+            $('.mobile.hamburger').show()  
+            $('.mobile.close').hide()   
+            $('#mobile_menu').hide()
+        }
+    }
 })
